66968f8b5b582ddcaa311ef859979656d6545757
Mark Holtman <mholtman@gmail.com>
2017-03-17 15:47:46 -0400

    Added setup for Guts tests

--- lib/vending_guts.ex
+++ lib/vending_guts.ex
@@ -1,6 +1,14 @@
 defmodule VendingGuts do
+  use GenServer
   @moduledoc """
   Backing GenServer for the vending machine
   """
+
+  @doc """
+  Starts up the guts
+  """
+  def start_link do
+    GenServer.start_link(__MODULE__, :ok, [])
+  end
 
 end
--- test/vending_guts_test.exs
+++ test/vending_guts_test.exs
@@ -1,6 +1,10 @@
 defmodule VendingGutsTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
   doctest VendingGuts
 
+  setup do
+    {:ok, guts} = VendingGuts.start_link()
+    {:ok, guts: guts}
+  end
 
 end
