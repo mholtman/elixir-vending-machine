7c6ca77e56462a2af242ad43ac21b716dedb0e1a
Mark Holtman <mholtman@gmail.com>
2017-09-25 14:12:43 -0400

    Bash wrapper around elixir container

---
+++ bin/run_elixir_command.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+main() {
+  COMMAND_RUN_ON_HOST=(docker run --rm)
+  append_tty_args_when_connected_to_tty
+  append_volume_and_working_dir
+  append_versioned_ansible_docker_image
+
+  COMMANDS_RUN_IN_CONTAINER="$*"
+
+  exec "${COMMAND_RUN_ON_HOST[@]}" /bin/sh -c "${COMMANDS_RUN_IN_CONTAINER[@]}"
+}
+
+append_tty_args_when_connected_to_tty() {
+  [[ -t 0 && -t 1 ]] && COMMMAND_RUN_ON_HOST+=(-it)
+}
+
+append_versioned_ansible_docker_image() {
+  COMMAND_RUN_ON_HOST+=("${DOCKER_IMAGE_OVERRIDE}")
+}
+
+append_volume_and_working_dir() {
+  COMMAND_RUN_ON_HOST+=(--net=bridge \
+                        --volume $(pwd):/work \
+                        --workdir /work)
+}
+
+main "$@"
